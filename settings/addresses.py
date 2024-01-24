a383c06f59252bccdeec0c8bc723eef22544b40a
Lukas Petrasek <lukas@carmine.finance>
2024-01-24 22:47:31 +0000

    add settings

---
+++ settings/addresses.py
@@ -0,0 +1,7 @@
+# Addresses of accounts for which we gather transactions.
+# Source: https://docs.solend.fi/Architecture/addresses.
+SOLEND_ADDRESS: str = 'So1endDq2YkqhipRh3WViPa8hdiSpxWy6z3Z6tMCpAo'
+# Source: https://github.com/blockworks-foundation/mango-v4.
+MANGO_ADDRESS: str = '4MangoMjqJ2firMokCjjGgoK8d4MXcrgL7XJaL3w6fVg'
+# Source: https://docs.kamino.finance/build-on-kamino/sdk-and-smart-contracts.
+KAMINO_ADDRESS: str = '6LtLpnUFNByNXLyCoK9wA2MykKAmQNZKBdY8s47dehDc'
