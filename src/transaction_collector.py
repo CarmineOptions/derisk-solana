c2438a7128e46cdf2fc9943b16d9eb229cbf4480
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-01-24 17:46:31 +0100

    db model for signatures, tx_collector

---
+++ src/transaction_collector.py
@@ -0,0 +1,103 @@
+import os
+import logging
+from typing import List
+
+import solana.rpc.api
+from solders.rpc.responses import RpcConfirmedTransactionStatusWithSignature
+from solders.signature import Signature
+
+from db_model import get_db_session, TransactionStatusWithSignature, TransactionStatusError, TransactionStatusMemo
+
+LOGGER = logging.getLogger(__name__)
+
+
+# pk = '6LtLpnUFNByNXLyCoK9wA2MykKAmQNZKBdY8s47dehDc'
+PPK = '4MangoMjqJ2firMokCjjGgoK8d4MXcrgL7XJaL3w6fVg'
+
+TX_BATCH_SIZE = 1000
+
+
+class TransactionCollector:
+    def __init__(self, protocol_public_key: str):
+        self.protocol_public_key = protocol_public_key
+        self.solana_client = solana.rpc.api.Client(os.getenv("QUICKNODE_TOKEN"))
+
+        self._last_tx_signature: Signature | None = None
+        self._last_tx_hit: bool = False
+
+    def _fetch_transactions(self) -> List[RpcConfirmedTransactionStatusWithSignature]:
+        """
+        Fetch transaction signatures.
+        Fetch only transactions appeared before self._last_tx_signature. If None - fetch starting from the latest.
+        :return: list of transactions.
+        """
+        response = self.solana_client.get_signatures_for_address(
+            solana.rpc.api.Pubkey.from_string(self.protocol_public_key),
+            limit=TX_BATCH_SIZE,
+            before=self._last_tx_signature
+        )
+        transactions = response.value
+
+        if len(transactions) < TX_BATCH_SIZE:
+            self._last_tx_hit = True
+
+        return transactions
+
+    def _add_transactions_to_db(self, transactions: List[RpcConfirmedTransactionStatusWithSignature]) -> None:
+        """
+        Write transaction signatures to db
+        :param transactions: fetched transactions
+        """
+        with get_db_session() as session:
+            for transaction in transactions:
+                tx_status_record = TransactionStatusWithSignature(
+                    signature=str(transaction.signature),
+                    source=self.protocol_public_key,
+                    slot=transaction.slot,
+                    block_time=transaction.block_time
+                )
+                session.add(tx_status_record)
+                # store errors and/or memos to db if any
+                if transaction.err:
+                    tx_error_record = TransactionStatusError(
+                        error_body=transaction.err.to_json(),
+                        tx_signatures_id=tx_status_record.id,
+                    )
+                    session.add(tx_memo_record)
+                if transaction.memo:
+                    tx_memo_record = TransactionStatusMemo(
+                        memo_body=transaction.memo,
+                        tx_signatures_id=tx_status_record.id,
+                    )
+                    session.add(tx_memo_record)
+
+            session.commit()
+        self._last_tx_signature = transactions[-1]
+
+    def collect_transactions(self, last_tx_signature: Signature | None = None) -> Signature:
+        """
+        Fetch all transaction finilized before last_tx_signature.
+        :param last_tx_signature:
+        :return: Last available signature
+        """
+        LOGGER.info(f"Start collecting transactions for `{self.protocol_public_key}`.")
+        if last_tx_signature:
+            self._last_tx_signature = last_tx_signature
+
+        while not self._last_tx_hit:
+            transactions = self._fetch_transactions()
+            if not transactions:
+                break
+            self._add_transactions_to_db(transactions)
+
+        LOGGER.info(f'All available transactions are collected. The last signature: `{str(self._last_tx_signature)}`.')
+        return last_tx_signature
+
+
+if __name__ == '__main__':
+    tx_collector = TransactionCollector(protocol_public_key=PPK)
+
+    last_tx_sign = tx_collector.collect_transactions()
+
+    print(last_tx_sign)
+
