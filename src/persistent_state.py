8e91fd33820e4c9dcf1414efc99442aa6183872c
Lukas Petrasek <lukas@carmine.finance>
2024-01-30 14:10:29 +0000

    save persistent state

---
+++ src/persistent_state.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+
+
+# TODO: To be defined.
+LAST_UPDATE_FILENAME: str = ''
+
+
+# TODO: To be implemented.
+def upload_object_as_pickle(
+	object: Any,
+	path: str,
+) -> None:
+	pass
