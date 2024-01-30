a6253bbbc2ad85c529db991e66df80c0d00e12cc
Lukas Petrasek <lukas@carmine.finance>
2024-01-30 23:38:48 +0000

    update persistent state

--- src/persistent_state.py
+++ src/persistent_state.py
@@ -1,9 +1,16 @@
 from typing import Any
 
+import src.protocols.kamino
+import src.protocols.mango
+import src.protocols.solend
+import src.protocols.state
+
 
 
 # TODO: To be defined.
 LAST_UPDATE_FILENAME: str = ''
+# TODO: To be defined.
+PERSISTENT_STATE_DIRECTORY: str = ''
 
 
 # TODO: To be implemented.
@@ -11,4 +18,18 @@ def upload_object_as_pickle(
 	object: Any,
 	path: str,
 ) -> None:
+	pass
+
+
+# TODO: To be implemented.
+def load_states() -> dict[str, src.protocols.state.State]:
+	return {
+		'Kamino': src.protocols.kamino.KaminoState(),
+		'Mango': src.protocols.mango.MangoState(),
+		'Solend': src.protocols.solend.SolendState(),
+	}
+
+
+# TODO: To be implemented.
+def load_pickle(path: str) -> Any:
 	pass
