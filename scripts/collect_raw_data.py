b307be5d57e0f9819827010a37d67fbace17368f
Lukas Petrasek <lukas@carmine.finance>
2024-01-31 21:29:47 +0000

    update script

--- scripts/collect_raw_data.py
+++ scripts/collect_raw_data.py
@@ -1,5 +1,5 @@
-import argparse
 import logging
+import os
 import sys
 
 sys.path.append(".")
@@ -12,15 +12,17 @@ import src.transaction_collector
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    # Parse arguments.
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--rpc_token', type=str, required=False, help='RPC access token')
-    parser.add_argument('-r', '--rate', type=int, required=False, help='Rate limit')
-    args = parser.parse_args()
+    RPC_TOKEN = os.environ.get("RPC_TOKEN")
+    if RPC_TOKEN is None:
+        raise ValueError("no RPC_TOKEN env var")
+    RATE_LIMIT = os.environ.get("RATE_LIMIT")
+    if RATE_LIMIT is None:
+        raise ValueError("no RATE_LIMIT env var")
+    RATE_LIMIT = int(RATE_LIMIT)
 
     # Collect signatures and transactions for each lending protocol since origin.
     src.transaction_collector.collect_signatures_and_transactions(
         start_signatures={address: None for address in src.protocols.addresses.ALL_ADDRESSES.values()},
-        rpc_token=args.rpc_token, 
-        rate=args.rate, 
+        rpc_token=RPC_TOKEN,
+        rate=RATE_LIMIT,
     )
