b5b3557b90d707e267a914cf63e6ba2cc49e9ea1
Chepelau <chepelau.a@gmail.com>
2024-02-22 13:23:27 +0100

    Remove trailing whitespace

--- scripts/collect_ob_liq.py
+++ scripts/collect_ob_liq.py
@@ -8,7 +8,7 @@ import asyncio
 import sys
 sys.path.append(".")
 
-from src.protocols.dexes.dexes import update_ob_dex_data_continuously # pylint: disable=C0413 
+from src.protocols.dexes.dexes import update_ob_dex_data_continuously # pylint: disable=C0413
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
