7a1791471b7a7143676a87c6649bf456bc48bd77
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-02-27 16:53:36 +0100

    async block fetching

--- collection/tx_data/collector.py
+++ collection/tx_data/collector.py
@@ -1,11 +1,15 @@
 """
 Class for collection of transaction data from Solana chain
 """
+import asyncio
 from abc import abstractmethod
-from typing import List
+from typing import List, Tuple
 import logging
 import os
+import time
 
+from solana.exceptions import SolanaRpcException
+from solana.rpc.async_api import AsyncClient
 from solana.rpc.commitment import Commitment
 from solders.transaction_status import EncodedTransactionWithStatusMeta, UiConfirmedBlock
 from sqlalchemy.exc import IntegrityError
@@ -23,20 +27,82 @@ class TXFromBlockCollector(GenericSolanaConnector):
     """
     protocol_public_keys: List[str] | None = None
 
+    def __init__(self):
+        super().__init__()
+        self.async_solana_client = AsyncClient(self.authenticated_rpc_url)
+
+    async def _async_rate_limit_calls(self) -> None:
+        """
+        This method implements a rate-limiting mechanism to control the frequency of API calls asynchronously.
+        It ensures that the number of API calls does not exceed a predefined limit per second.
+        If the limit is reached, it will pause the execution momentarily before allowing further API calls.
+        """
+        # Check if the number of calls made in the last second exceeds the maximum limit.
+        while len(self._call_timestamps) >= self.rate_limit:
+            # Calculate the time passed since the first call in the timestamp list.
+            time_passed = time.time() - self._call_timestamps[0]
+
+            if time_passed > 1:
+                # More than a second has passed since the first call, remove it from the list.
+                self._call_timestamps.pop(0)
+            else:
+                # Wait for the remainder of the second before allowing more calls, asynchronously.
+                await asyncio.sleep(1 - time_passed)
+
+        # Record the timestamp of the current call.
+        self._call_timestamps.append(time.time())
+
+    async def run(self):
+        """
+        Main method to process all necessary operations to collect and write data.
+        """
+        k = 0
+        start_time = time.time()
+        while not self._collection_completed:
+            if k % 10000 == 0:
+                LOG.info(f"Iterations completed: {k}, in {time.time() - start_time:.1f} seconds.")
+            self._get_assignment()
+            await self._get_data()
+            self._write_tx_data()
+            k += 1
+        LOG.info(f"Collection completed for {self.__class__.__name__}.")
+
     @property
     @abstractmethod
     def COLLECTION_STREAM(self) -> db.CollectionStreamTypes:  # pylint: disable=invalid-name
         """Implement in subclasses to define the constant value"""
         raise NotImplementedError("Implement me!")
 
-    def _get_data(self):
+    async def _get_data(self) -> None:
         """
         Collect transactions from blocks. Collected transactions are stored in `rel_transactions` attribute.
         """
         self.rel_transactions.clear()
-        for block_number in self.assignment:
-            block = self._fetch_block(block_number)
+        # Use asyncio.gather to fetch blocks concurrently
+        blocks = await asyncio.gather(
+            *(self._async_fetch_block(block_number) for block_number in self.assignment)
+        )
+        for block, block_number in blocks:
             self._select_relevant_tx_from_block(block, block_number)
+
+    async def _async_fetch_block(self, block_number: int) -> Tuple[UiConfirmedBlock, int]:
+        """
+        Use solana client to fetch block with provided number.
+        """
+        await self._async_rate_limit_calls()
+        # Fetch block data.
+        try:
+            block = await self.async_solana_client.get_block(
+                slot=block_number,
+                encoding='jsonParsed',
+                max_supported_transaction_version=0,
+            )
+        except SolanaRpcException as e:
+            LOG.error(f"SolanaRpcException while fetching {block_number}: {e}")
+            await asyncio.sleep(0.5)
+            return await self._async_fetch_block(block_number)
+
+        return block.value, block_number
 
     def _select_relevant_tx_from_block(self, block: UiConfirmedBlock, block_number: int) -> None:
         """
@@ -62,6 +128,7 @@ class TXFromBlockCollector(GenericSolanaConnector):
         self._get_protocol_public_keys()
         # receive block numbers ready for collection
         self._get_assigned_blocks()
+        LOG.info(f"Blocks for collection: {self.assignment}")
 
     @abstractmethod
     def _get_assigned_blocks(self) -> None:
@@ -112,8 +179,12 @@ class TXFromBlockCollector(GenericSolanaConnector):
         """
         Fetches number of the last finalized block.
         """
-        self._rate_limit_calls()
-        return self.solana_client.get_slot(commitment=Commitment('finalized')).value
+        try:
+            return self.solana_client.get_slot(commitment=Commitment('finalized')).value
+        except SolanaRpcException as e:
+            LOG.error(f"SolanaRpcException while fetching the last finalized block on chain: {e}")
+            time.sleep(0.5)
+            return self._get_latest_finalized_block_on_chain()
 
     def _write_tx_data(self) -> None:
         """
