f9942acf8025bc837b2c3cc6b0fd51610b4f1ac6
Chepelau <chepelau.a@gmail.com>
2024-02-29 20:49:50 +0100

    Add Sentre to ts-fetcher

--- ts-fetcher/src/main.ts
+++ ts-fetcher/src/main.ts
@@ -1,33 +1,33 @@
 
 
-import './sentre.js';
+import { sleep } from './utils.js';
 
-// import { sleep } from './utils.js';
+import { updateSaberLiqudity } from './saber.js';
+import { updateInvariantLiqudity } from './invariant.js';
+import { updateLifinityLiqudity } from './lifinity.js';
+import { updateSentreLiqudity } from './sentre.js';
 
-// import { updateSaberLiqudity } from './saber.js';
-// import { updateInvariantLiqudity } from './invariant.js';
-// import { updateLifinityLiqudity } from './lifinity.js';
+// Collect data every 5 minutes
+const COLLECTION_INTERVAL_MILLISECONDS = 5 * 1 * 1000;
 
-// // Collect data every 5 minutes
-// const COLLECTION_INTERVAL_MILLISECONDS = 5 * 60 * 1000;
+async function main() {
 
-// async function main() {
+    while (true) {
+        const start = Date.now();
 
-//     while (true) {
-//         const start = Date.now();
+        // Update Liquidity
+        await updateInvariantLiqudity();
+        await updateSaberLiqudity();
+        await updateLifinityLiqudity();
+        await updateSentreLiqudity();
 
-//         // Update Liquidity
-//         await updateInvariantLiqudity();
-//         await updateSaberLiqudity();
-//         await updateLifinityLiqudity();
+        // Sleep until the end of collection interval
+        const execution_time = Date.now() - start;
+        const to_sleep = Math.max(0, COLLECTION_INTERVAL_MILLISECONDS - execution_time);
 
-//         // Sleep until the end of collection interval
-//         const execution_time = Date.now() - start;
-//         const to_sleep = Math.max(0, COLLECTION_INTERVAL_MILLISECONDS - execution_time);
+        await sleep(to_sleep);
+    };
 
-//         await sleep(to_sleep);
-//     };
+}
 
-// }
-
-// await main()
+await main()
--- ts-fetcher/src/sentre.ts
+++ ts-fetcher/src/sentre.ts
@@ -1,15 +1,67 @@
+import { Keypair } from "@solana/web3.js";
+import { solana_connection } from "./utils.js";
+import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
+import SenswapLib from "@sentre/senswap/dist/lib/program.js";
+import { AmmLiquidityEntry, writeAmmLiquidityEntries } from './db.js';
 
-import { Keypair } from '@solana/web3.js';
-import { solana_connection } from './utils.js';
-import { AnchorProvider, Wallet } from '@coral-xyz/anchor';
+type MyObject = {
+  default: any;
+};
 
+const SENTRE_IDENTIFIER = "SENTRE";
+const Senswap = (SenswapLib as unknown as MyObject).default;
 
-import Senswap from '@sentre/senswap';
 const sth = new Senswap(
-    new AnchorProvider(
-        solana_connection,
-        new Wallet(Keypair.generate()),
-        {}
-    ),
-    ''
+  new AnchorProvider(solana_connection, new Wallet(Keypair.generate()), {}),
+  "D3BBjqUdCYuP18fNvvMbPAZ8DpcRi4io2EsYHQawJDag"
 );
+
+const RELEVANT_MARKETS = {
+    "SOL-USDC" : {"publicKey":"CT2QmamF6kBBDVbkg8WkvF5gnq6q8mDranPi21tdGeeL", "tokenXdecimals": 9, "tokenYdecimals": 6},
+    "SNTR-USDC": {"publicKey":"2gtDG2iYam6z4eCjx9yfBD7ayRXQGTDymjqQLiHqr7Z6", "tokenXdecimals": 9, "tokenYdecimals": 6},
+    "C98-USDC" : {"publicKey":"13Jn5xugRGjVorHWakzjvdZBMFwPLQniKHRoE6j4BMCC", "tokenXdecimals": 6, "tokenYdecimals": 6},
+    "ETH-USDC" : {"publicKey":"AzPdQteHNWLvgRtFQX2N9K2U14M7rwub4VjEeKhaSbuh", "tokenXdecimals": 8, "tokenYdecimals": 6}
+}
+
+
+async function getSentreAmmLiquidityEntries(): Promise<AmmLiquidityEntry[]> {
+
+    const relevantMarketsValues = Object.entries(RELEVANT_MARKETS);
+    const pools: AmmLiquidityEntry[] = [];
+
+    for (let i = 0; i < relevantMarketsValues.length; i++) {
+        const [name, poolInfo] = relevantMarketsValues[i];
+        
+        let pool = await sth.getPoolData(poolInfo['publicKey']);
+
+        const entry: AmmLiquidityEntry = {
+            timestamp: Math.floor(Date.now() / 1_000),
+            dex: SENTRE_IDENTIFIER,
+            pair: name,
+            market_address: poolInfo['publicKey'],
+            token_x: pool.reserves[0].toString(),
+            token_y: pool.reserves[1].toString(),
+            token_x_decimals: poolInfo['tokenXdecimals'],
+            token_y_decimals: poolInfo['tokenYdecimals'],
+            additional_info: JSON.stringify({
+                "tokenXweight": pool.weights[0].toNumber(),
+                "tokenYweight": pool.weights[1].toNumber(),
+            })
+        };
+
+        pools.push(entry);
+    }
+    return pools;
+}
+
+export async function updateSentreLiqudity() {
+  try {
+    const poolUpdates = await getSentreAmmLiquidityEntries();
+    await writeAmmLiquidityEntries(poolUpdates);
+
+    console.log('Updated Sentre Liquidity');
+  } catch (e) {
+    console.log("Encountered error while updating Sentre liquidity: ");
+    console.log(e);
+  }
+}
