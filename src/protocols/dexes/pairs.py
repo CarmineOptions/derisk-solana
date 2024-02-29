f7a2f2c807235cb7356eb6309e372723c36370a0
Chepelau <chepelau.a@gmail.com>
2024-02-29 18:39:21 +0100

    Add more Openbook and Phoenix pairs

--- src/protocols/dexes/pairs.py
+++ src/protocols/dexes/pairs.py
@@ -42,10 +42,60 @@ PAIRS: dict[str, dict[str, str]] = {
         "PHOENIX": "5x91Aaegvx1JmW7g8gDfWqwb6kPF7CdNunqNoYCdLjk1",
         "OPENBOOK": "H6Wvvx5dpt8yGdwsqAsz9WDkT43eQUHwAiafDvbcTQoQ",
     },
+    "PYTH/USDC": {
+        "PHOENIX": "2sTMN9A1D1qeZLF95XQgJCUPiKe5DiV52jLfZGqMP46m",
+        "OPENBOOK": "EA1eJqandDNrw627mSA1Rrp2xMUvWoJBz2WwQxZYP9YX",
+    },
+    "BONK/USDC": {
+        "PHOENIX": "GBMoNx84HsFdVK63t8BZuDgyZhSBaeKWB4pHHpoeRM9z",
+        'OPENBOOK': "7tV5jsyNUg9j1AARv56b7AirdpLBecibRXLEJtycEgpP"
+    },
+    "BONK/SOL": {
+        "PHOENIX": "FicF181nDsEcasznMTPp9aLa5Rbpdtd11GtSEa1UUWzx",
+        "OPENBOOK": "Hs97TCZeuYiJxooo3U73qEHXg3dKpRL4uYKYRryEK9CF",
+    },
+    "WIF/USDC": {
+        "PHOENIX": "6ojSigXF7nDPyhFRgmn3V9ywhYseKF9J32ZrranMGVSX",
+        "OPENBOOK":"2BtDHBTCTUxvdur498ZEcMgimasaFrY5GzLv8wS8XgCb"
+    },
+    "JitoSOL/USDC": {
+        "PHOENIX": "5LQLfGtqcC5rm2WuGxJf4tjqYmDjsQAbKo2AMLQ8KB7p",
+        "OPENBOOK": "DkbVbMhFxswS32xnn1K2UY4aoBugXooBTxdzkWWDWRkH",
+    },
+    "DUAL/USDC": {"OPENBOOK": "H6rrYK3SUHF2eguZCyJxnSBMJqjXhUtuaki6PHiutvum"},
+    "RAY/USDC": {"OPENBOOK": "DZjbn4XC8qoHKikZqzmhemykVzmossoayV9ffbsUqxVj"},
+    "stSOL/USDC": {"OPENBOOK": "JCKa72xFYGWBEVJZ7AKZ2ofugWPBfrrouQviaGaohi3R"},
+    "LDO/USDC": {"OPENBOOK": "BqApFW7DwXThCDZAbK13nbHksEsv6YJMCdj58sJmRLdy"},
+    "stSOL/SOL": {"OPENBOOK": "GoXhYTpRF4vs4gx48S7XhbaukVbJXVycXimhGfzWNGLF"},
+    "JitoSOL/SOL": {"OPENBOOK": "G8KnvNg5puzLmxQVeWT2cRHCm1XmurbWGG9B8Bze6mav"},
+    "HNT/USDC": {"OPENBOOK": "CK1X54onkDCqVnqY7hnvhcT7EosnjiLTwPBXAMLxkA2A"},
+    "bSOL/USDC": {"OPENBOOK": "ARjaHVxGCQfTvvKjLd7U7srvk6orthZSE6uqWchCczZc"},
+    "bSOL/SOL": {"OPENBOOK": "6QNusiQ1g7fKierMQhNeAJxfLXomfcAX3tGRMwxfESsw"},
+    "KIN/USDC": {"OPENBOOK": "4WeAXG1V8QTtt3T9ao6LkQa8m1AuwRcY8YLvVcabiuby"},
+    "CHAI/USDC": {"OPENBOOK": "7S2fEFvce5n9hGpjp9jd8JRfuBngcDJfykygeqqzEwmq"},
+    "ALL/USDC": {"OPENBOOK": "EN41nj1uHaTHmJJLPTerwVu2P9r5G8pMiNvfNX5V2PtP"},
+    "RLB/USDC": {"OPENBOOK": "72h8rWaWwfPUL36PAFqyQZU8RT1V3FKG7Nc45aK89xTs"},
+    "CROWN/USDC": {"OPENBOOK": "HDwpKCNpB9JvcGrZv6TWcXjFvzxxfzq7ci6kQ1Kv8FMY"},
+    "TBTC/USDC": {"OPENBOOK": "6nh2KwhGF8Tott22smj2E3G1R15iXhBrL7Lx6vKgdPFK"},
+    "GUAC/USDC": {"OPENBOOK": "63XwffQkMcNqEacDNhixmBxnydkRE3uigV7VoLNfqh9k"},
+    "NOS/USDC": {"OPENBOOK": "9LezACAkFsv78P7nBJEzi6QeF9h1QF8hGx2LRN7u9Vww"},
+    "NEON/USDC": {"OPENBOOK": "Fb5BfdB7zk2zfWfqgpRtRQbYSYERASsBjz213FaT461F"},
+    "SLCL/USDC": {"OPENBOOK": "AuqKXU1Nb5XvRxr5A4vRBLnnSJrdujNJV7HWsfj4KBWS"},
+    "JTO/USDC": {"OPENBOOK": "H87FfmHABiZLRGrDsXRZtqq25YpARzaokCzL1vMYGiep"},
+    "SAMO/USDC": {"OPENBOOK": "E5AmUKMFgxjEihVwEQNrNfnri5EexYHSBC4HkicVtfxG"},
+    "CORN/USDC": {"OPENBOOK": "2mBnnBywAuMwH5FhH27UUFyDGk7J77m5LcKK4VtmwJQi"},
+    "TBTC/wBTCpo": {"OPENBOOK": "3rQH87K3UfrDjbjSktHy7EwQHvX4BoRu3Py52D25gKSS"},
+    "RENDER/USDC": {"OPENBOOK": "2m7ZLEKtxWF29727DSb5D91erpXPUY1bqhRWRC3wQX7u"},
+    "MNDE/USDC": {"OPENBOOK": "CC9VYJprbxacpiS94tPJ1GyBhfvrLQbUiUSVMWvFohNW"},
+    "JLP/USDC": {"OPENBOOK": "ASUyMMNBpFzpW3zDSPYdDVggKajq1DMKFFPK1JS9hoSR"},
+    "GECKO/USDC": {"OPENBOOK": "8QCdRwLp5CX2XYVaKX3GFxsbc8n7M2xEtMXyAa8tL7r3"},
+    "WEN/USDC": {"OPENBOOK": "2oxZZ3YXaVhbZmtzagGooewBAofyVbBTzayAD9UR1eBh"},
+    "MOUTAI/USDC": {"OPENBOOK": "74fKpZ1NFfusLacyVzQdMXXawe9Dr1Kz8Yw1cw12QQ3y"},
+    "BLZE/USDC": {"OPENBOOK": "GFJjJmm7jTDb7WEM4TkYdA9eAEeJGK1t73tcdDNeZLGT"},
+    "GOFX/USDC": {"OPENBOOK": "9FjM1wHvGg2ZZaB3XyRsYELoQE7iD6uwHXizQUDKRYff"},
+    "ELON/USDC": {"OPENBOOK": "CDm1Uaos4vWPXezgEobUarGJ6ddKCywvFp8XLcNSqzU9"},
+    "STEP/USDC": {"OPENBOOK": "CCepXEQxo8eTqCGtRHXrSnZdhCEQjQeEW3M85AH9skMJ"},
 }
-
-# NOTES: wETH  - from Wormhole bridge
-# NOTES: ETHpo - from Portal bridge
 
 
 def get_relevant_tickers(dex_identifier: str) -> dict[str, str]:
