e27ad778de42b201e27daafc1d446e568fa1d64c
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-03-14 16:20:27 +0100

    fix src/protocols/idl_paths.py:1:53: C0303: Trailing whitespace

--- src/protocols/idl_paths.py
+++ src/protocols/idl_paths.py
@@ -1,4 +1,4 @@
-# Paths to IDLs which help with parsing transactions. 
+# Paths to IDLs which help with parsing transactions.
 # Source: TODO.
 SOLEND_IDL_PATH: str = 'TODO'
 # Source: https://github.com/blockworks-foundation/mango-v4/blob/dev/mango_v4.json.
