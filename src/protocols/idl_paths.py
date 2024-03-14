122600d234192934356542608f0aaa0736532370
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-03-14 15:46:09 +0100

    add all relevant idls, remove irrelevant, fix kamino public key

--- src/protocols/idl_paths.py
+++ src/protocols/idl_paths.py
@@ -3,5 +3,7 @@
 SOLEND_IDL_PATH: str = 'TODO'
 # Source: https://github.com/blockworks-foundation/mango-v4/blob/dev/mango_v4.json.
 MANGO_IDL_PATH: str = 'src/protocols/idls/mango_v4.json'
-# Source: https://solscan.io/account/6LtLpnUFNByNXLyCoK9wA2MykKAmQNZKBdY8s47dehDc#anchorProgramIDL.
-KAMINO_IDL_PATH: str = 'src/protocols/idls/yvaults.json'
+# Source: https://explorer.solana.com/address/KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD/anchor-program
+KAMINO_IDL_PATH: str = 'src/protocols/idls/kamino_idl.json'
+# Source: https://explorer.solana.com/address/MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA/anchor-program
+MARGINFI_IDL_PATH: str = 'src/protocols/idls/marginfi_idl.json'
