943f01b534847d2221b2669c6ca4bfd0c792f551
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-04-11 12:04:05 +0200

    optimise parsing script

--- scripts/marginfi_transaction_parser.py
+++ scripts/marginfi_transaction_parser.py
@@ -30,34 +30,39 @@ def process_transactions():
         Pubkey.from_string(MARGINFI_ADDRESS)
     )
     while True:
-        # get transactions that are not yet parsed
+        transactions_to_update = []
+        transactions_data = []
+
+        # Minimize the DB session scope to only fetching necessary data
         with get_db_session() as session:
-
-            # Select a batch of transactions that haven't been parsed yet
             transactions = session.query(MarginfiTransactionsList).filter_by(is_parsed=False).limit(BATCH_SIZE).all()
+            LOGGER.info(f"Fetched {len(transactions)} transactions for processing.")
 
-            LOGGER.info(f"Start processing {len(transactions)} transactions.")
             for transaction in transactions:
-                # Fetch the corresponding transaction_data from table for transactions.
                 tx_data = session.query(TransactionStatusWithSignature.transaction_data).filter(
                     TransactionStatusWithSignature.signature == transaction.signature
                 ).first()
-
                 if tx_data and tx_data.transaction_data:
-                    # get transaction from JSON
-                    transaction_data = EncodedTransactionWithStatusMeta.from_json(tx_data.transaction_data)
+                    transactions_data.append((transaction, tx_data.transaction_data))
 
-                    # Decode the transaction data
-                    # Decode the transaction data
-                    tx_decoder._processor = lambda x: tx_decoder.save_event_to_database(x, timestamp=transaction.block_time)
-                    tx_decoder.decode_tx(transaction_data)
+        # Process transactions outside the DB session
+        for transaction, tx_data_json in transactions_data:
+            transaction_data = EncodedTransactionWithStatusMeta.from_json(tx_data_json)
+            # Set up the decoder processor function
+            tx_decoder._processor = lambda x: tx_decoder.save_event_to_database(x, timestamp=transaction.block_time)
+            # Decode the transaction data
+            tx_decoder.decode_tx(transaction_data)
+            # Collect transactions that have been successfully parsed
+            transactions_to_update.append(transaction.id)
 
-                    # Mark the transaction as parsed
-                    transaction.is_parsed = True
-
-            # Commit the changes once all transactions in the batch have been processed
-            session.commit()
-        LOGGER.info("Transaction batch has been successfully parsed.")
+        # Update the database in a single operation to mark transactions as parsed
+        if transactions_to_update:
+            with get_db_session() as session:
+                session.query(MarginfiTransactionsList).filter(
+                    MarginfiTransactionsList.id.in_(transactions_to_update)).update({"is_parsed": True},
+                                                                                    synchronize_session='fetch')
+                session.commit()
+                LOGGER.info(f"Successfully parsed and updated {len(transactions_to_update)} transactions.")
 
 
 if __name__ == '__main__':
