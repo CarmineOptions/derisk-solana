b18b58fe846ee2381ec031eb3173469c5a79a919
Chepelau <chepelau.a@gmail.com>
2024-04-11 14:36:10 +0200

    Add setup for liq normalization

---
+++ src/protocols/dexes/liquidity_normalization/normalization.py
@@ -0,0 +1,11 @@
+import time
+import logging
+
+
+LOG = logging.getLogger(__name__)
+
+def normalize_dex_data_continuously():
+    while True:
+        LOG.info('TODO: Implement Me!')
+        time.sleep(5)
+
