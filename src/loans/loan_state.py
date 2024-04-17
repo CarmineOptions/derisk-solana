d25b7d332d11edc43ba468bd82b284a70a36a1c9
DaveVodrazka <dave.vodrazka@gmail.com>
2024-04-17 22:18:32 +0200

    feat: split per protocol logic

--- src/loans/loan_state.py
+++ src/loans/loan_state.py
@@ -1,18 +1,81 @@
 import logging
 import time
+from typing import Callable, Literal
+
 import pandas
 from sqlalchemy import func
 from sqlalchemy.orm.session import Session
 from db import (
-    LoanStates,
+    MangoLoanStates,
+    MarginfiLoanStates,
+    KaminoLoanStates,
     MangoParsedTransactions,
     MarginfiParsedTransactions,
     KaminoParsedTransactions,
     get_db_session,
 )
 
+MARGIFI = "margifi"
+MANGO = "mango"
+KAMINO = "kamino"
 
-def store_loan_states(df: pandas.DataFrame, session: Session):
+Protocol = Literal["margifi", "mango", "kamino"]
+
+AnyParsedTransactions = (
+    MangoParsedTransactions | MarginfiParsedTransactions | KaminoParsedTransactions
+)
+
+
+def process_margifi_events(
+    events: list[MarginfiParsedTransactions],
+) -> pandas.DataFrame:
+    # TODO: process margify events
+    print(events)
+
+    return pandas.DataFrame()
+
+
+def process_mango_events(events: list[MangoParsedTransactions]) -> pandas.DataFrame:
+    # TODO: process mango events
+    print(events)
+
+    return pandas.DataFrame()
+
+
+def process_kamino_events(events: list[KaminoParsedTransactions]) -> pandas.DataFrame:
+    # TODO: process kamino events
+    print(events)
+
+    return pandas.DataFrame()
+
+
+def protocol_to_process_func(
+    protocol: Protocol,
+) -> Callable[[list[AnyParsedTransactions]], pandas.DataFrame]:
+    if protocol == MARGIFI:
+        return process_margifi_events
+    elif protocol == MANGO:
+        return process_mango_events
+    elif protocol == KAMINO:
+        return process_kamino_events
+    else:
+        raise ValueError(f'Invalid protocol "{protocol}"')
+
+
+def protocol_to_model(
+    protocol: Protocol,
+) -> MangoLoanStates | MarginfiLoanStates | KaminoLoanStates:
+    if protocol == MARGIFI:
+        return MarginfiLoanStates
+    elif protocol == MANGO:
+        return MangoLoanStates
+    elif protocol == KAMINO:
+        return KaminoLoanStates
+    else:
+        raise ValueError(f'Invalid protocol "{protocol}"')
+
+
+def store_loan_states(df: pandas.DataFrame, protocol: Protocol, session: Session):
     """
     Stores data from a pandas DataFrame to the loan_states table.
 
@@ -27,8 +90,10 @@ def store_loan_states(df: pandas.DataFrame, session: Session):
     - session (sqlalchemy.orm.session.Session): A SQLAlchemy session object.
     """
 
+    model = protocol_to_model(protocol)
+
     for _, row in df.iterrows():
-        loan_state = LoanStates(
+        loan_state = model(
             slot=row["slot"],
             protocol=row["protocol"],
             user=row["user"],
@@ -39,7 +104,7 @@ def store_loan_states(df: pandas.DataFrame, session: Session):
     session.commit()
 
 
-def fetch_loan_states(session: Session) -> pandas.DataFrame:
+def fetch_loan_states(protocol: Protocol, session: Session) -> pandas.DataFrame:
     """
     Fetches loan states with the max slot from the DB and returns them as a DataFrame
 
@@ -55,13 +120,13 @@ def fetch_loan_states(session: Session) -> pandas.DataFrame:
         - debt (json/dict): JSON or dictionary representing debt.
     """
 
+    model = protocol_to_model(protocol)
+
     # Define a subquery for the maximum slot value
-    max_slot_subquery = session.query(func.max(LoanStates.slot)).subquery()
+    max_slot_subquery = session.query(func.max(model.slot)).subquery()
 
     # Retrieve entries from the loan_states table where slot equals the maximum slot value
-    query_result = (
-        session.query(LoanStates).filter(LoanStates.slot == max_slot_subquery).all()
-    )
+    query_result = session.query(model).filter(model.slot == max_slot_subquery).all()
 
     df = pandas.DataFrame(
         [
@@ -79,7 +144,7 @@ def fetch_loan_states(session: Session) -> pandas.DataFrame:
 
 
 def fetch_events(
-    min_slot: int, protocol: str, session: Session
+    min_slot: int, protocol: Protocol, session: Session
 ) -> MarginfiParsedTransactions | MangoParsedTransactions | KaminoParsedTransactions:
     """
     Fetches loan states with the max slot from the DB and returns them as a DataFrame
@@ -113,43 +178,31 @@ def fetch_events(
     raise ValueError(f"invalid protocol {protocol}")
 
 
-def process_events_to_loan_states(session: Session):
-    current_loan_states = fetch_loan_states(session)
+def process_events_to_loan_states(
+    protocol: Protocol,
+    process_function: Callable[[list[AnyParsedTransactions]], pandas.DataFrame],
+    session: Session,
+):
+    current_loan_states = fetch_loan_states(protocol, session)
     min_slot = 0
 
     if len(current_loan_states) > 0:
         min_slot = int(current_loan_states.iloc[0]["slot"])
 
-    mango_events: list[MangoParsedTransactions] = fetch_events(
-        min_slot, "mango", session
-    )
-    marginfi_events: list[MarginfiParsedTransactions] = fetch_events(
-        min_slot, "marginfi", session
-    )
-    kamino_events: list[KaminoParsedTransactions] = fetch_events(
-        min_slot, "kamino", session
-    )
+    events: list[MarginfiParsedTransactions] = fetch_events(min_slot, protocol, session)
 
-    new_loan_state = pandas.DataFrame()
+    new_loan_state = process_function(events)
 
-    for event in mango_events:
-        # TODO: process mango events
-        pass
+    store_loan_states(new_loan_state, protocol, session)
 
-    for event in marginfi_events:
-        # TODO: process marginfi events
-        pass
 
-    for event in kamino_events:
-        # TODO: process kamino events
-        pass
-
-    store_loan_states(new_loan_state, session)
-
-def process_events_continuously():
+def process_events_continuously(protocol: Protocol):
     logging.info("Starting events to loan_states processing.")
     session = get_db_session()
+
+    process_func = protocol_to_process_func(protocol)
+
     while True:
-        process_events_to_loan_states(session)
+        process_events_to_loan_states(protocol, process_func, session)
         logging.info("Updated loan_states.")
         time.sleep(120)
