97c9ccef453af7a55259e1da9aabd37f5c668338
DaveVodrazka <dave.vodrazka@gmail.com>
2024-04-18 18:28:28 +0200

    feat: add solend to loan states

--- scripts/process_events_to_loan_state.py
+++ scripts/process_events_to_loan_state.py
@@ -8,7 +8,7 @@ from src.loans.loan_state import process_events_continuously
 
 if __name__ == "__main__":
     protocol = sys.argv[1]
-    valid_protocols = {"marginfi", "mango", "kamino"}
+    valid_protocols = {"marginfi", "mango", "kamino", "solend"}
     if protocol not in valid_protocols:
         raise ValueError(f"{protocol} is not a valid protocol")
 
