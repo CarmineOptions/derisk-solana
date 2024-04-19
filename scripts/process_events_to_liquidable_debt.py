941a8bcb26ec8d036393241cfcb167c7e7e3419e
DaveVodrazka <dave.vodrazka@gmail.com>
2024-04-19 13:34:13 +0200

    feat: dockerize liquidable debts

---
+++ scripts/process_events_to_liquidable_debt.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+sys.path.append(".")
+
+from src.loans.liquidable_debt import process_events_continuously
+
+
+if __name__ == "__main__":
+    protocol = sys.argv[1]
+    valid_protocols = {"marginfi", "mango", "kamino", "solend"}
+    if protocol not in valid_protocols:
+        raise ValueError(f"{protocol} is not a valid protocol")
+
+    logging.basicConfig(level=logging.INFO)
+
+    process_events_continuously(protocol)
