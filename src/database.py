bea0346ca741490ac612904a73089a81e52d90d3
Chepelau <chepelau.a@gmail.com>
2024-04-21 19:18:26 +0200

    Raise ValueError instead of info log in src/database.py

--- src/database.py
+++ src/database.py
@@ -5,12 +5,23 @@ import sys
 import psycopg2
 
 
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+if POSTGRES_USER is None:
+    raise ValueError("no POSTGRES_USER env var")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+if POSTGRES_PASSWORD is None:
+    raise ValueError("no POSTGRES_PASSWORD env var")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+if POSTGRES_HOST is None:
+    raise ValueError("no POSTGRES_HOST env var")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+if POSTGRES_DB is None:
+    raise ValueError("no POSTGRES_DB env var")
 
-PG_CONNECTION_STRING = os.environ.get("PG_CONNECTION_STRING")
+PG_CONNECTION_STRING = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+
 if PG_CONNECTION_STRING is None:
-    logging.info("No PG connection string, aborting.")
-    sys.exit()
-
+    raise ValueError("No PG connection string, aborting.")
 
 
 def establish_connection() -> psycopg2.extensions.connection:
