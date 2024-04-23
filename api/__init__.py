6b89faa4c89ea4a02b6cc8c8d7b32e378a89dde6
DaveVodrazka <dave.vodrazka@gmail.com>
2024-04-23 09:51:20 +0200

    fix: compression and logging

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from flask import Flask
 
@@ -11,6 +12,9 @@ from api.v1 import v1
 
 # init app
 application = Flask(__name__)
+
+# set endpoints
+application.register_blueprint(v1, url_prefix="/v1")
 
 # init cache
 cache.init_app(application)
@@ -27,10 +31,12 @@ application.config["COMPRESS_MIN_SIZE"] = 500
 compress.init_app(application)
 
 # init GCP logging
-client = google.cloud.logging.Client()
-client.setup_logging()
-
-# set endpoints
-application.register_blueprint(v1, url_prefix="/v1")
+# set "USE_GCP_LOGGING" to anything to allow GCP logging
+if os.environ.get("USE_GCP_LOGGING") is not None:
+    client = google.cloud.logging.Client()
+    client.setup_logging()
+    logging.info("logging into GCP logs")
+else:
+    logging.info("using default logging")
 
 logging.info("API started")
