fb4adacb9774e311582f1db165a596bd9b660004
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-04-25 00:00:27 +0200

    add data_pipeline to scripts/parsing_pipeline.py

--- scripts/parsing_pipeline.py
+++ scripts/parsing_pipeline.py
@@ -1,12 +1,78 @@
 import sys
+import logging
 
-import sqlalchemy
+from sqlalchemy import text
 
 from db import get_db_session
+from src.protocols.addresses import MANGO_ADDRESS, MARGINFI_ADDRESS, KAMINO_ADDRESS, SOLEND_ADDRESS
+
+LOGGER = logging.getLogger(__name__)
+BATCH_SIZE = 10
+
 
 def data_pipeline(protocol):
-     with get_db_session() as session:
-         pass
+    if protocol == 'marginfi':
+        protocol_pubkey = MARGINFI_ADDRESS
+        transaction_reporting_table = 'marginfi_hist_transaction_list_v2'
+    elif protocol == 'solend':
+        protocol_pubkey = SOLEND_ADDRESS
+        transaction_reporting_table = 'solend_hist_transaction_list'
+    elif protocol == 'kamino':
+        protocol_pubkey = KAMINO_ADDRESS
+        transaction_reporting_table = 'kamino_hist_transaction_list_v3'
+    elif protocol == 'mango':
+        protocol_pubkey = MANGO_ADDRESS
+        transaction_reporting_table = 'mango_hist_transaction_list_v3'
+
+    else:
+        raise Exception(f"Wrong protocol selection: {protocol}")
+
+    while True:
+
+        with get_db_session() as session:
+            last_record = session.execute(text(f"""
+                 SELECT * FROM lenders.{transaction_reporting_table} ORDER BY id DESC limit 1;
+             """)).fetchall()
+
+            last_signature = last_record[0].signature
+
+        with get_db_session() as session:
+            last_collected_transaction = session.execute(text(f"""
+                 SELECT *
+                 FROM lenders.transactions
+                 WHERE signature = '{last_signature}'
+                     AND source = '{protocol_pubkey}'
+                 limit 1;
+             """)).fetchall()
+            last_block = last_collected_transaction[0].slot
+
+        with get_db_session() as session:
+            batch_to_collect = session.execute(text(f"""
+                 SELECT distinct slot
+                 FROM lenders.transactions
+                 WHERE 
+                     slot > {last_block}
+                     AND source = '{protocol_pubkey}'
+                 ORDER BY slot
+                 LIMIT {BATCH_SIZE};
+             """)).fetchall()
+            blocks_batch = [r.slot for r in batch_to_collect]
+
+        with get_db_session() as session:
+            session.execute(text(f"""
+                 INSERT INTO lenders.{transaction_reporting_table} (signature, block_time, is_parsed)
+                 SELECT 
+                     signature, 
+                     MIN(block_time) AS block_time,
+                     FALSE AS is_parsed
+                 FROM lenders.transactions
+                 WHERE source = '{protocol_pubkey}' 
+                 AND slot >= {min(blocks_batch)}
+                 AND slot <= {max(blocks_batch)}
+                 GROUP BY signature
+                 ORDER BY MIN(block_time);
+             """))
+            session.commit()
 
 
 if __name__ == "__main__":
@@ -15,3 +81,4 @@ if __name__ == "__main__":
     if protocol not in valid_protocols:
         raise ValueError(f"{protocol} is not a valid protocol")
 
+    data_pipeline(protocol)
