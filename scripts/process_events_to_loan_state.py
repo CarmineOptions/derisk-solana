97fefa5899b0859b649a0e8b6c5a74125e2b31f4
Lukas Petrasek <lukas@carmine.finance>
2024-05-13 14:29:47 +0000

    improve logging setup

--- scripts/process_events_to_loan_state.py
+++ scripts/process_events_to_loan_state.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     if protocol not in valid_protocols:
         raise ValueError(f"{protocol} is not a valid protocol")
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     process_events_continuously(protocol)
