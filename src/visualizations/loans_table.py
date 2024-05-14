fa2e498b670653a5ed503f6d2ded75ff5d28d0dd
Chepelau <chepelau.a@gmail.com>
2024-05-14 19:31:45 +0000

    Fetch health ratios from db on fe

--- src/visualizations/loans_table.py
+++ src/visualizations/loans_table.py
@@ -1,12 +1,18 @@
 import decimal
-from typing import Literal, Callable
-import pandas
+from typing import Literal, Callable, Type
+import pandas as pd
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import aliased
+from sqlalchemy.sql import func
+
 import src.loans.mango
 import src.loans.kamino
 import src.loans.marginfi
 import src.loans.solend
 
+
 from src.loans.loan_state import fetch_loan_states
+
 import db
 
 
@@ -25,31 +31,110 @@ KAMINO = "kamino"
 SOLEND = "solend"
 Protocol = Literal["marginfi", "mango", "kamino", "solend"]
 
+AnyHealthRatioModel = (
+    Type[db.MangoHealthRatios]
+    # | Type[db.MarginfiHealthRatios]
+    # | Type[db.KaminoHealthRatios]
+    # | Type[db.SolendHealthRatios]
+)
 
-def get_loan_states_handler(protocol: Protocol) -> Callable:
+def get_health_ratio_protocol_model(protocol: Protocol) -> AnyHealthRatioModel | None:
+
 	if protocol == MANGO:
-		return src.loans.mango.get_mango_user_stats_df
+		return db.MangoHealthRatios
 
 	if protocol == KAMINO:
-		return src.loans.kamino.get_kamino_user_stats_df
+		return db.KaminoHealthRatios
 
 	if protocol == MARGINFI:
-		return src.loans.marginfi.get_marginfi_user_stats_df
+		return db.MarginfiHealthRatios
 		
 	if protocol == SOLEND:
-		return src.loans.solend.get_solend_user_stats_df
+		return db.SolendHealthRatios
+
+	return None
+
+def fetch_health_ratios(session: Session, model: AnyHealthRatioModel) -> pd.DataFrame:
+	"""
+	Note: Does not close the session.
+	"""
+	subquery = session.query(
+		model.user,
+		func.max(model.timestamp).label('latest_timestamp')
+	).group_by(model.user).subquery()
+
+	# Join the subquery with the main table to get the latest entries
+	latest_entries = session.query(db.MangoHealthRatios).join(
+		subquery,
+		(db.MangoHealthRatios.user == subquery.c.user) & (db.MangoHealthRatios.timestamp == subquery.c.latest_timestamp)
+	).all()
+
+	data = [{
+		'slot': entry.slot,
+		'last_update': entry.last_update,
+		'user': entry.user,
+		'health_factor': entry.health_factor,
+		'std_health_factor': entry.std_health_factor,
+		'collateral': entry.collateral,
+		'risk_adjusted_collateral': entry.risk_adjusted_collateral,
+		'debt': entry.debt,
+		'risk_adjusted_debt': entry.risk_adjusted_debt,
+		'protocol': entry.protocol,
+		'timestamp': entry.timestamp
+	} for entry in latest_entries]
+
+	return pd.DataFrame(data)
 		
-		
-def load_user_stats_data(protocols) -> pandas.DataFrame:
+def load_user_health_ratios(protocols: list[str]) -> pd.DataFrame:
+	"""
+	For list of protocols it returns a dataframe containg all users health ratios.
+	"""
 
 	data = []
 
 	with db.get_db_session() as sesh:
 		for protocol in protocols:
-			state_handler = get_loan_states_handler(protocol)
-			loan_states = fetch_loan_states(protocol, sesh)
+			
+			# Get the correct model for given protocol
+			model = get_health_ratio_protocol_model(protocol)
 
-			df = state_handler(loan_states)
-			data.append(df)
-		
-	return pandas.concat(data)
+			if not model:
+				logging.error(f'Unable to find HealthRatios model for protocol "{protocol}"')
+				continue
+
+			# Fetch health ratios data
+			health_ratios_df = fetch_health_ratios(sesh, model)
+			# Drop redundant columns
+			health_ratios_df = health_ratios_df.drop(['timestamp', 'slot', 'last_update'], axis =1)
+			# Rename some collumns that are also present in loan states 
+			# (in loan states these collumns contain the dict with debt/collateral holdings)
+			health_ratios_df = health_ratios_df.rename(columns={'collateral': 'collateral_usd', 'debt': 'debt_usd'})
+
+			# Fetch loan states 
+			loan_states_df = fetch_loan_states(protocol, sesh)
+			# Select only needed columns
+			loan_states_df = loan_states_df[['user', 'collateral', 'debt']]
+
+			# Merge health ratios and loan states so we have user holdings
+			# also present in the table
+			full_df = health_ratios_df.merge(loan_states_df, on = 'user')
+			# Rename collumns to make it nicer
+			full_df = full_df.rename(columns={
+				'user': 'User',
+				'protocol': 'Protocol',
+				'health_factor': 'Health Factor',
+				'std_health_factor': 'Standardized Health Factor',
+				'collateral_usd': 'Collateral (USD)',
+				'risk_adjusted_collateral': 'Risk. Adj. Collateral',
+				'debt_usd': 'Debt (USD)',
+				'risk_adjusted_debt': 'Risk. Adj. Debt',
+				'collateral': 'Collaterals',
+				'debt': 'Debts'
+			})
+
+			data.append(full_df)
+
+	return pd.concat(data).set_index('User')
+
+
+
