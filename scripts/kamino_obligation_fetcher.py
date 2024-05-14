4666707d2337de31e8dbdfcaaf5c851786596da5
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-05-14 17:55:43 +0200

    reserves colletcion

--- scripts/kamino_obligation_fetcher.py
+++ scripts/kamino_obligation_fetcher.py
@@ -1,25 +1,29 @@
 """
-Script for Solend loan states collection.
+Script for Kamino loan states collection.
 """
 # Standard library imports
 import logging
 import os
 import time
+from pathlib import Path
 from typing import Any, Dict, List, Tuple
 
 # Related third-party imports
 import pandas as pd
-import requests
 from solana.exceptions import SolanaRpcException
 from solana.rpc.api import Client
 from solana.rpc.types import MemcmpOpts
 from solders.pubkey import Pubkey
-from solders.rpc.responses import GetProgramAccountsResp
+from solders.rpc.responses import RpcKeyedAccount
 
 # Local application/library specific imports
 import db
 from src.loans.loan_state import store_loan_states
+from src.parser import TransactionDecoder
+from src.protocols.addresses import KAMINO_ADDRESS
 from src.protocols.anchor_clients.kamino_client.accounts.obligation import Obligation
+from src.protocols.anchor_clients.kamino_client.accounts.reserve import Reserve
+from src.protocols.idl_paths import KAMINO_IDL_PATH
 
 # logger
 LOGGER = logging.getLogger(__name__)
@@ -27,48 +31,26 @@ LOGGER = logging.getLogger(__name__)
 # Constants
 KAMINO_PROGRAM_ID = Pubkey.from_string("KLend2g3cP87fffoy8q1mQqGKjrxjC8boSyAYavgmjD")
 AUTHENTICATED_RPC_URL = os.getenv("RPC_URL")
+LENDING_MARKET_MAIN = '7u3HeHxYDLhnCoErrtycNokbQYbWGzLs6JSDqGAv5PfF'
+JLP_MARKET = 'DxXdAyU3kCjnyggvHmY5nAwg5cRbbmdyX3npfDMjjMek'
+ALTCOIN_MARKET = 'ByYiZxp8QrdN9qbdtaAiePN8AAr3qvTPppNJDpf5DVJ5'
 
 
-
-# def get_reserve_to_supply_map(reserves: List[str]) -> Dict[str, Any]:
-#     """ Get reserve data from solend.fi API. """
-#     ids = ",".join(reserves)
-#     url = f'https://api.solend.fi/v1/reserves/?ids={ids}'
-#
-#     response = requests.get(url, timeout=15)
-#     return {
-#         i['reserve']['address']: {
-#             'liquiditySupply': i['reserve']['liquidity']['supplyPubkey'],
-#             'collateralSupply': i['reserve']['collateral']['supplyPubkey'],
-#             'liquidityMint': i['reserve']['liquidity']['mintPubkey'],
-#             'liquidityMintDecimals': i['reserve']['liquidity']['mintDecimals'],
-#             'collateralMint': i['reserve']['collateral']['mintPubkey'],
-#             'cumBorrowRateWADs': i['reserve']['liquidity']['cumulativeBorrowRateWads']
-#         }
-#         for i in response.json()['results']
-#     }
-
-
-def fetch_obligations(pool_pubkey: str, client: Client) -> GetProgramAccountsResp:
+def fetch_accounts(pool_pubkey: str, client: Client, filters: List[Any]) -> List[RpcKeyedAccount]:
     """ Fetch Solend obligations for pool. """
     try:
-
-        filters = [
-            Obligation.layout.sizeof() + 8,
-            MemcmpOpts(32, pool_pubkey),
-        ]
         response = client.get_program_accounts(
             KAMINO_PROGRAM_ID,
             encoding='base64',
             filters=filters
         )
 
-        return response
+        return response.value
 
     except SolanaRpcException as e:
         LOGGER.error(f"SolanaRpcException: {e} while collecting obligations for `{pool_pubkey}`.")
         time.sleep(0.5)
-        return fetch_obligations(pool_pubkey, client)
+        return fetch_accounts(pool_pubkey, client, filters)
 
 
 def get_slot_number(client: Client) -> int:
@@ -83,6 +65,77 @@ def get_slot_number(client: Client) -> int:
         return get_slot_number(client)
 
 
+def get_reserve_to_supply_map(decoder: TransactionDecoder, client: Client) -> Dict[str, Any]:
+    """ Get reserve data from solend.fi API. """
+    reserves = []
+    for market in [LENDING_MARKET_MAIN, JLP_MARKET, ALTCOIN_MARKET]:
+        filters = [
+            Reserve.layout.sizeof() + 8,
+            MemcmpOpts(32, str(market)),
+        ]
+
+        market_accounts = fetch_accounts(market, client, filters)
+
+        market_reserves = [
+            {
+                'address': str(reserve.pubkey),
+                'info': decoder.program.coder.accounts.decode(reserve.account.data)
+            }
+            for reserve in market_accounts
+        ]
+
+        for reserve in market_reserves:
+            reserves.append(reserve)
+
+    return {
+        reserve['address']: {
+            'liquiditySupply': str(reserve['info'].liquidity.supply_vault),
+            'collateralSupply': str(reserve['info'].collateral.supply_vault),
+            'liquidityMint': str(reserve['info'].liquidity.mint_pubkey),
+            'liquidityMintDecimals': reserve['info'].liquidity.mint_decimals,
+            'collateralMint': str(reserve['info'].collateral.mint_pubkey),
+            'cumBorrowRateWADs': reserve['info'].liquidity.cumulative_borrow_rate_bsf.value[0]
+        }
+        for reserve in reserves
+    }
+
+
+def process_collateral(obligation: Any, reserve_to_supply_map: Dict[str, Any]):
+    """
+    Format collateral data:
+     - replace reserve with corresponding supply
+    """
+    collateral_raw = obligation.deposits
+    collateral = {}
+    for position in collateral_raw:
+        reserve = position.deposit_reserve
+        collateral_mint = reserve_to_supply_map[reserve]['collateralMint']
+        collateral[collateral_mint] = {'amount': position.deposited_amount, 'reserve': reserve}
+    return collateral
+
+
+def process_debt(obligation: Any, reserve_to_supply_map: Dict[str, Any]):
+    """
+    Format debt data:
+    - replace reserve with corresponding supply
+    - calculate borrowed amount by accounting for interest.
+    """
+    debt_raw = obligation.borrows
+    debt = {}
+    for position in debt_raw:
+        reserve = position.borrow_reserve
+        liquidity_mint = reserve_to_supply_map[reserve]['liquidityMint']
+        borrowed_amount = position.borrowed_amount_sf
+        cumulative_borrow_rate = position.cumulative_borrow_rate_bsf.value[0]
+
+        debt[liquidity_mint] = {
+            'rawAmount': borrowed_amount / cumulative_borrow_rate,
+            'reserve': reserve
+        }
+
+    return debt
+
+
 def obtain_loan_states():
     """ Obtain Solend loan states. """
     LOGGER.info("Start loan states collection.")
@@ -90,35 +143,35 @@ def obtain_loan_states():
     client = Client(AUTHENTICATED_RPC_URL)
     slot = get_slot_number(client)
     # get and decode main pool obligations
-    response = fetch_obligations(str(LENDING_MARKET_MAIN), client)
-    decoded_obligations = {
-        str(i.pubkey): decode_obligation(ObligationLayout.parse(i.account.data))
-        for i in response.value
-    }
-    LOGGER.info("Loan states for Solend MAIN pool successfully collected and decoded.")
+    obligations = fetch_accounts(
+        str(LENDING_MARKET_MAIN), client, filters = [
+            Obligation.layout.sizeof() + 8,
+            MemcmpOpts(32, str(LENDING_MARKET_MAIN)),
+        ]
+    )
+
+    LOGGER.info("Loan states for KAMINO MAIN pool successfully collected and decoded.")
     # get and decode obligations for isolated pools
-    for _, market_address in ISOLATED_POOLS.items():
-        response = fetch_obligations(market_address, client)
-        if not response.value:
-            continue
-        decoded_pool_obligations = {
-            str(i.pubkey): decode_obligation(ObligationLayout.parse(i.account.data))
-            for i in response.value
-        }
-        decoded_obligations.update(decoded_pool_obligations)
-    LOGGER.info("Loan states for Solend ISOLATED pools successfully collected and decoded.")
-    # get supply data for reserves
-    reserves = list({
-        i['publicKey']
-        for _, obligation in decoded_obligations.items()
-        for position in list(obligation)
-        for i in position
-    })
-    reserve_to_supply_map = get_reserve_to_supply_map(reserves)
+    for market_address in [JLP_MARKET, ALTCOIN_MARKET]:
+        filters = [
+            Obligation.layout.sizeof() + 8,
+            MemcmpOpts(32, market_address),
+        ]
+        response = fetch_accounts(market_address, client=client, filters=filters)
+        obligations.extend(response)
+    LOGGER.info("Loan states for KAMINO ALTCOIN and JLP pools successfully collected.")
+    # Create Kamino program decoder
+    decoder = TransactionDecoder(path_to_idl=Path(KAMINO_IDL_PATH), program_id=Pubkey.from_string(KAMINO_ADDRESS))
+    decoded_obligations = []
+    # decode obligations
+    for obligation in obligations:
+        decoded_obligations.append(decoder.program.coder.accounts.decode(obligation.account.data))
+    # decode reserves and create reserve_to_supply_map
+    reserve_to_supply_map = get_reserve_to_supply_map(decoder, client)
 
     # Format decoded obligation data
     obligations_processed = []
-    for user, obligation in decoded_obligations.items():
+    for user, obligation in decoded_obligations:
         processed_obligation = {
             'slot': slot,
             'protocol': 'solend',
