334646ff8f69ee0fcdadec08cd2298f08041bc39
Chepelau <chepelau.a@gmail.com>
2024-05-15 17:57:56 +0000

    Move duplicate code to shared.py

---
+++ src/visualizations/shared.py
@@ -0,0 +1,37 @@
+from typing import Type, Literal
+
+import db
+
+# TODO: This is duplicate, and it's used somewhere else in the repo,
+# move this somewhere more sensible and use everywhere
+MARGINFI = "marginfi"
+MANGO = "mango"
+KAMINO = "kamino"
+SOLEND = "solend"
+Protocol = Literal["marginfi", "mango", "kamino", "solend"]
+
+AnyHealthRatioModel = (
+    Type[db.MangoHealthRatio]
+    | Type[db.SolendHealthRatio]
+    # | Type[db.MarginfiHealthRatio]
+    # | Type[db.KaminoHealthRatio]
+)
+
+def get_health_ratio_protocol_model(protocol: Protocol) -> AnyHealthRatioModel | None:
+
+	if protocol == MANGO:
+		return db.MangoHealthRatio
+
+	if protocol == KAMINO:
+		return None
+		# return db.KaminoHealthRatio
+
+	if protocol == MARGINFI:
+		return None
+		# return db.MarginfiHealthRatio
+		
+	if protocol == SOLEND:
+		return db.SolendHealthRatio
+
+	return None
+
