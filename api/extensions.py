053b2c6f897335b3bcc9abd580ac3d039e04b32d
DaveVodrazka <dave.vodrazka@gmail.com>
2024-05-17 14:42:37 +0200

    fix: remove Base2

--- api/extensions.py
+++ api/extensions.py
@@ -2,9 +2,8 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from flask_compress import Compress
 
-from db import Base, Base2
+from db import Base
 
 cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 db = SQLAlchemy(model_class=Base)
-db2 = SQLAlchemy(model_class=Base2)
 compress = Compress()
