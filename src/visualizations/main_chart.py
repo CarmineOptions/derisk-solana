ff08df482598ee121651c6ba4ca11b32e20ba434
nikanor.goreglyad <nikanor.goreglyad@creativedock.com>
2024-05-21 16:38:10 +0200

    fix cta message formating

--- src/visualizations/main_chart.py
+++ src/visualizations/main_chart.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass
 import datetime
 # import math
@@ -174,10 +175,8 @@ def get_liquidable_debt_single_protocol(
         return df
 
     except ValueError:
-        logging.error(f"No data for {_db_model}")
+        logging.error(f"No data for pair: {collateral_token_address} - {debt_token_address}")
         return None
-
-    
 
 
 @st.cache_data(ttl=datetime.timedelta(minutes=60))
